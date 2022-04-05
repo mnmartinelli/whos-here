3aae04956a5fc7501ee7f1a7a1b2f5fa7dd55a3c
Mike Wagner <mvw5669@psu.edu>
2022-04-05 00:21:09 -0400

    some db endpoints

---
+++ api/updateUsername.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+const mysql = require('mysql2')
+
+//
+//
+//needs some work
+export default async function handler(req, res) {
+    const { oldUsername, newUsername } = req.query;
+
+    var updatedUser = {
+        username: newUsername,
+    };
+
+    const connection = mysql.createConnection(process.env.DATABASE_URL);
+    
+    //get the ID somewhere up here to compare below
+    //updateUser.id = dbResult.insertId;
+
+    //is this how updates work?
+    const [dbResult] = await connection.execute(
+        `UPDATE users
+            SET
+                username = :newUsername
+            WHERE
+                username = :oldUsername`
+    );
+
+    res.json(user);
+}
